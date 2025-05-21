60f83ee03cb9b6d074d38660c57bed3cc9561451
rakibulhassan07 <rakibalhassan523@gmail.com>
2025-05-21 13:58:45 +0600

    added firebase auth

--- src/main.jsx
+++ src/main.jsx
@@ -8,13 +8,16 @@ import {
 } from "react-router-dom";
 import { router } from './Router/Routes.jsx';
 import { HelmetProvider } from 'react-helmet-async';
+import AuthProvider from './provider/AuthProvider.jsx';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HelmetProvider>
+    <AuthProvider>
+      <HelmetProvider>
       <div className='max-w-screen-xl mx-auto'>
       <RouterProvider router={router} />
     </div>
     </HelmetProvider>
+    </AuthProvider>
       
   </StrictMode>,
 )
---
+++ src/provider/AuthProvider.jsx
@@ -0,0 +1,60 @@
+import { createContext, useEffect, useState } from "react";
+import {signInWithPopup, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile } from "firebase/auth";
+import { app } from '../firebase/firebase.init';
+
+export const AuthContext =createContext(null);
+const auth = getAuth(app);
+const AuthProvider = ({children}) => {
+
+    const [user,setUser]=useState(null);
+    const [loading,setLoading]=useState(true);
+    //create user
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+      };
+      const updateUserProfile = (name, photo) => {
+        setLoading(true);
+        return updateProfile(auth.currentUser, {
+          displayName: name,
+          photoURL: photo,
+        });
+      };
+        
+      const signIn=(email,password)=>{
+        setLoading(true);
+        return signInWithEmailAndPassword(auth,email,password);
+    }
+      const logOut=()=>{
+        setLoading(true);
+        return signOut(auth);
+    }
+
+      useEffect(()=>{
+        const unSubscribe=onAuthStateChanged(auth,currentUser=>{
+
+            //console.log('user in auth ',currentUser)
+            setUser(currentUser);
+            setLoading(false);
+        });
+        return unSubscribe;
+    },[]) 
+     
+        const authInfo={
+             user,
+             setUser,
+             createUser,
+             loading,
+             logOut,
+             signIn,
+             updateUserProfile
+        } 
+    return (
+        <AuthContext.Provider value={authInfo}> 
+            {children}
+        </AuthContext.Provider>
+            
+    );
+};
+
+export default AuthProvider;
